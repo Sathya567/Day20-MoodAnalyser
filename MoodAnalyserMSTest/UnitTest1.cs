1d97805bd3b7f713137ce33894ea8b05817c9d13
BootCampBL <satyabhandari43@gmail.com>
2021-08-06 13:03:37 +0530

    [satya] added-exception handling if user input invalid mood it returns null

--- MoodAnalyserMSTest/UnitTest1.cs
+++ MoodAnalyserMSTest/UnitTest1.cs
@@ -6,6 +6,7 @@ namespace MoodAnalyserMSTest
     [TestClass]
     public class UnitTest1
     {
+        #region TC 1.1
         [TestMethod]
         public void GivenMoodHappy_ShouldReturnHappy()
         {
@@ -13,15 +14,29 @@ namespace MoodAnalyserMSTest
             string result = obj.analyseMood();
             Assert.AreEqual("SAD", result);
         }
+        #endregion
 
+
+        #region TC 1.2
         [TestMethod]
         public void GivenMoodSad_ShouldReturnSad()
         {
-            MoodAnalyser obj = new MoodAnalyser("I am in sad mood");
+            MoodAnalyser obj = new MoodAnalyser("I am in sad mood ");
             string result = obj.analyseMood();
             Assert.AreEqual("SAD", result);
         }
+        #endregion
+
+
+        #region TC 2.1
+        [TestMethod]
+        [ExpectedException(typeof(MoodAnalyserCustomException))]
+        public void GivenMoodNull_ShouldThrowException()
+        {
+            MoodAnalyser obj = new MoodAnalyser(null);
+            string result = obj.analyseMood();
+            Assert.AreEqual("HAPPY", result);
+        }
+        #endregion
     }
-
 }
-

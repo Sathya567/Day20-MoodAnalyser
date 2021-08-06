12b25898f410235f6547621a8bd088246f57a84e
BootCampBL <satyabhandari43@gmail.com>
2021-08-06 13:02:17 +0530

    [satya] added-exception handling if user input invalid mood it returns null

--- Day_20_MSTestMoodAnalyser/MoodAnalyser.cs
+++ Day_20_MSTestMoodAnalyser/MoodAnalyser.cs
@@ -16,12 +16,25 @@ namespace Day_20_MSTestMoodAnalyser
         }
         public string analyseMood()
         {
-            if (this.message.Contains("sad"))
-                return "SAD";
-            else if (this.message.Contains("happy"))
-                return "SAD";
-            else
-                return "HAPPY";
+            try
+            {
+                if (this.message.Equals(string.Empty))
+                {
+                    throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.EMPTY_MESSAGE, "Mood should not be empty");
+                }
+                if (this.message.Contains("sad"))
+                    return "SAD";
+                else if (this.message.Contains("happy"))
+                    return "SAD";
+                else
+                    return "HAPPY";
+
+            }
+            catch (NullReferenceException)
+            {
+                throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NULL_MESSAGE, "Mood should not be null");
+            }
         }
     }
 }
+
